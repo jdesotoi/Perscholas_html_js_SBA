b04a7d2a70aa6595debe80d2d39be481bb672f04
Jorge De Soto <jorge.desoto@gmail.com>
2022-09-20 00:18:35 -0400

    Fixed error on email registration

--- js/item.js
+++ js/item.js
@@ -1,7 +1,8 @@
 import { shop,  menu  } from "/js/initialData.js";
-import { PrintMenu, printOneProduct, getUrlParams, getAdd } from "/js/function.js";
+import { PrintMenu, printOneProduct, getUrlParams, getAdd,getItFresh } from "/js/function.js";
 
 PrintMenu(menu);
 let sku = getUrlParams().get("SKU");
 printOneProduct(sku, shop);
 getAdd();
+getItFresh();
